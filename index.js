4652311e5a293bf14dc25679183b5a384bfafa3f
atticuscornett <64325242+atticuscornett@users.noreply.github.com>
2024-07-23 01:31:00 -0500

    Start custom level download work

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const { app, BrowserWindow, ipcMain } = require('electron')
 const path = require("path");
 const fs = require("fs");
+const {get} = require("https");
 let customLevelFolder = "C:\\Program Files\\Oculus\\Software\\Software\\hyperbolic-magnetism-beat-saber\\Beat Saber_Data\\CustomLevels";
+
 
 function scanCustomLevels(){
     let folders = fs.readdirSync(customLevelFolder)
@@ -19,8 +21,39 @@ function scanCustomLevels(){
     return levels;
 }
 
+function downloadCustomLevel(id){
+    if (id.includes("/")){
+        return;
+    }
+    fetch("https://api.beatsaver.com/maps/id/" + id)
+        .then(response => response.json())
+        .then((data) => {
+            let fileUrl = data.versions[0].downloadURL;
+            const destination = 'beat-saber-' + id + '.zip';
+
+            const file = fs.createWriteStream(destination);
+
+            get(fileUrl, (response) => {
+                response.pipe(file);
+                file.on('finish', () => {
+                    file.close(() => {
+                        console.log('File downloaded successfully');
+                    });
+                });
+            }).on('error', (err) => {
+                fs.unlink(destination, () => {
+                    console.error('Error downloading file:', err);
+                });
+            });
+        })
+}
+
 ipcMain.handle("scanCustomLevels", (event, args) => {
     return scanCustomLevels();
+});
+
+ipcMain.handle("downloadCustomLevel", (event, args) => {
+    downloadCustomLevel(args);
 });
 
 console.log(JSON.stringify(scanCustomLevels()));
--- preload.js
+++ preload.js
@@ -1,5 +1,6 @@
 const {contextBridge, ipcRenderer} = require('electron');
 
 contextBridge.exposeInMainWorld('electronAPI', {
-    scanCustomLevels: () => ipcRenderer.invoke("scanCustomLevels")
+    scanCustomLevels: () => ipcRenderer.invoke("scanCustomLevels"),
+    downloadCustomLevel: (id) => ipcRenderer.invoke("downloadCustomLevel", id)
 });
