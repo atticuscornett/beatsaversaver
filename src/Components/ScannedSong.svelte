737adac82672e6c04687cd6c0388bad3ba59f6b4
atticuscornett <64325242+atticuscornett@users.noreply.github.com>
2024-07-23 01:13:48 -0500

    Show downloaded songs

---
+++ src/Components/ScannedSong.svelte
@@ -0,0 +1,51 @@
+<script>
+    export let song;
+</script>
+
+<div class="songResult">
+    <div class="imageContainer">
+        <img class="albumCover" src="{song.fullImagePath}" alt="Song Cover">
+    </div>
+    <div>
+        <h4>{song._songName}</h4>
+        <h5>{song._songAuthorName}</h5>
+        <h5>{song._levelAuthorName}</h5>
+    </div>
+    <div class="sideDetails">
+        <h5>{Math.round(song._beatsPerMinute)} bpm</h5>
+    </div>
+</div>
+
+<style>
+    div {
+        background: #222222;
+        border-radius: 10px;
+
+    }
+
+    h3, h4, h5 {
+        margin: 5px;
+    }
+
+    .songResult {
+        display: flex;
+        direction: ltr;
+        flex: 1 1 10px;
+        min-width: 600px;
+        margin: 8px;
+        padding: 5px;
+    }
+
+    .albumCover {
+        height: 100px;
+        border-radius: 8px;
+    }
+
+    .imageContainer {
+        text-align: center;
+    }
+
+    .sideDetails {
+        margin-left: auto;
+    }
+</style>
--- src/DownloadPage.svelte
+++ src/DownloadPage.svelte
@@ -1,1 +1,31 @@
 <h1>Downloads</h1>
+
+<script>
+    import {onMount} from "svelte";
+    import ScannedSong from "./Components/ScannedSong.svelte";
+
+    let downloadedLevels;
+
+    onMount(async () =>{
+        downloadedLevels = await electronAPI.scanCustomLevels();
+    })
+</script>
+
+<div id="downloadList">
+    {#if downloadedLevels}
+        {#each downloadedLevels as level}
+            <ScannedSong song={level}/>
+        {/each}
+    {:else}
+        <h1>Scanning downloaded levels...</h1>
+    {/if}
+</div>
+
+<style>
+    #downloadList {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: flex-start;
+        align-items: flex-start;
+    }
+</style>
