69937219acb71d0e10f49a3df4dbcf0d229995b7
atticuscornett <64325242+atticuscornett@users.noreply.github.com>
2024-07-23 23:20:46 -0500

    Create Level Info section

--- src/Components/LevelInfo.svelte
+++ src/Components/LevelInfo.svelte
@@ -5,6 +5,29 @@
         levelView = false;
     }
 
+    function songDuration(){
+        let duration = levelView.metadata.duration;
+        let minutes = Math.floor(duration / 60);
+        let seconds = duration - minutes * 60;
+        if (seconds < 10) {
+            seconds = `0${seconds}`;
+        }
+        return `${minutes}:${seconds}`;
+    }
+
+    function checkMods(){
+        // Mods - Chroma, Noodle Extensions, Mapping Extensions, Cinema, Any
+        let mods = [false, false, false, false];
+        mods[0] = levelView.versions[0].diffs[0].chroma;
+        mods[1] = levelView.versions[0].diffs[0].ne;
+        mods[2] = levelView.versions[0].diffs[0].me;
+        mods[3] = levelView.versions[0].diffs[0].cinema;
+        mods[4] = mods[0] || mods[1] || mods[2] || mods[3];
+        return mods;
+    }
+
+    let modList = checkMods();
+
     let splitDescription = levelView.description.split("\n");
 </script>
 
@@ -18,17 +41,64 @@
     <div class="headerTitles">
         <h2>{levelView.name}</h2>
         <h3>{levelView.metadata.songName} - {levelView.metadata.songAuthorName}</h3>
-        <h4>{levelView.metadata.levelAuthorName}</h4>
+        <h4>Mappers: {levelView.metadata.levelAuthorName}</h4>
     </div>
 </div>
 
-<div class="levelInfo">
+<div class="levelDetails">
     <details>
         <summary>Level Description</summary>
         {#each splitDescription as line}
             <p>{line}</p>
         {/each}
     </details>
+
+    <div class="levelInfo">
+        <h3>Level Info</h3>
+        <div class="levelInfoFlex">
+            <div class="levelInfoSpace">
+                <h4>Metadata</h4>
+                <h5>Level ID: {levelView.id}</h5>
+                <h5>BPM: {levelView.metadata.bpm}</h5>
+                <h5>Song Length: {songDuration()}</h5>
+            </div>
+
+            <div class="levelInfoSpace">
+                <h4>Tags</h4>
+                {#each levelView.tags as tag}
+                    <h5>{tag}</h5>
+                {/each}
+            </div>
+
+            <div class="levelInfoSpace">
+                <h4>Mods</h4>
+
+                {#if modList[0]}
+                    <h5>Chroma</h5>
+                {/if}
+
+                {#if modList[1]}
+                    <h5>Noodle Extensions</h5>
+                {/if}
+
+                {#if modList[2]}
+                    <h5>Mapping Extensions</h5>
+                {/if}
+
+                {#if modList[3]}
+                    <h5>Cinema</h5>
+                {/if}
+
+                {#if !modList[4]}
+                    <h5>None</h5>
+                {/if}
+            </div>
+        </div>
+    </div>
+
+    <div>
+        <h3>Community Info</h3>
+    </div>
 </div>
 
 <style>
@@ -51,6 +121,10 @@
         margin: 5px;
     }
 
+    h5 {
+        font-weight: normal;
+    }
+
     .headerTitles {
         display: flex;
         justify-content: space-around;
@@ -58,7 +132,7 @@
         margin-left: 10px;
     }
 
-    .levelInfo {
+    .levelDetails {
         margin: 8px;
     }
 
@@ -74,5 +148,20 @@
         font-size: 24px;
         padding: 8px;
         cursor: pointer;
+    }
+
+    .levelInfo {
+        padding: 15px;
+        background: #222222;
+        border-radius: 8px;
+        margin-top: 8px;
+    }
+
+    .levelInfoFlex {
+        display: flex;
+    }
+
+    .levelInfoSpace {
+        margin-right: 100px;
     }
 </style>
