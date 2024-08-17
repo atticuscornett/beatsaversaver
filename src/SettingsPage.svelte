b87f4bfc93df6824453e43c6dac5f24487dd1ad6
atticuscornett <64325242+atticuscornett@users.noreply.github.com>
2024-08-16 22:17:07 -0500

    Add more context in settings

--- src/SettingsPage.svelte
+++ src/SettingsPage.svelte
@@ -22,9 +22,11 @@
 
 <div class="settingsMargin">
     <h2>Beat Saber Custom Level Folder</h2>
-    <h5>Typically C:\Program Files\Oculus\Software\Software\hyperbolic-magnetism-beat-saber\Beat Saber_Data\CustomLevels</h5>
+    <h5>Typically C:\Program Files\Oculus\Software\Software\hyperbolic-magnetism-beat-saber\Beat Saber_Data\CustomLevels in Meta Quest Link</h5>
     <input on:change={setCustomLevelFolder} id="customLevelFolder">
     <button on:click={selectCustomLevelFolder}>...</button>
+
+    <h2></h2>
 </div>
 
 
